5ea60935421f59f2c2dfe05d21f170a358397948
Air BKK <thanida.thitilertdecha@dx1app.com>
2020-07-13 20:01:26 +0700

    add todo page with textbox

---
+++ src/components/AddTodoItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const AddTodoItem = () => {
+    return <>
+    <input type="text" placeholder="Waht needs to be done?"></input>
+    </>
+}
+
+
+
+export default AddTodoItem;
---
+++ src/containers/Todo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddTodoItem from '../components/AddTodoItem';
+export const Todo = () => {
+    return (<>
+    <h2>Todos</h2>
+    <AddTodoItem></AddTodoItem>
+    </>);
+}
+
+export default Todo;
+ 
